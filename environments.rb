b44b1aa16f0c794348e793860ac02750855d57b4
Michael Young <michaeljy@gmail.com>
2014-02-22 12:33:15 -0600

    added heroku environment variables

--- environments.rb
+++ environments.rb
@@ -7,6 +7,11 @@ end
 configure :production do
   db = URI.parse(ENV['HEROKU_POSTGRESQL_BLUE_URL'] || 'postgres:///localhost/mydb')
 
+  $twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  $twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  $twitter_access_token = ENV['TWITTER_ACCESS_TOKEN']
+  $twitter_access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host     => db.host,
